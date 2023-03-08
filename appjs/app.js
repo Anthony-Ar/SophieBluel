79429587e9436e18320c63699a2cac54a9899c5e
Anthony <anthony.arrighi@orange.fr>
2023-03-08 18:11:25 +0100

    #

--- appjs/app.js
+++ appjs/app.js
@@ -2,7 +2,6 @@ import displayAdminInterface from "./admin/interface.js";
 import { displayFilters } from "./components/filters.js";
 import { isConnected } from "./components/login.js";
 import { displayPortfolio } from "./components/portfolio.js";
-import { fetchApi } from "./utils/fetch.js";
 
 globalThis.currentFile = '';
 globalThis.createList = [];
